fdc9430e96b7a338cab1b3a9382ba734c0e2ff6d
Rodrigo Soares <soaresjw27@gmail.com>
2024-12-03 21:56:35 -0300

    bem vindo

--- testeGit/src/testegit/TesteGit.java
+++ testeGit/src/testegit/TesteGit.java
@@ -7,6 +7,8 @@ public class TesteGit {
     
     public static void main(String[] args) {
         System.out.println("oiiii");
+        System.out.println("bem vindo");
+        System.out.println("ao git");
     }
     
 }
